034898af26d95d19f32e2073d1f8f9a2a6f521e7
Kenryoguji <axelteuweussie@gmail.com>
2026-08-07 18:18:03 +0200

    add commented on code

--- save_local.py
+++ save_local.py
@@ -1,22 +1,24 @@
 import csv # import csv Library
 from pathlib import Path # Import Library Path for track
-import tkinter as tk # Graphicd Library
+import tkinter as tk # Graphic Library
 from tkinter import filedialog # windows dialog file
 
 root = tk.Tk() # Create a window named root
 root.withdraw() # cash the windows
 dossier = filedialog.askdirectory(title = "Sélectionner un répertoire") 
 
-if dossier :
-    track = Path(dossier)/"données.csv"
+if dossier : # si le répertoire est sélectionnée
+    track = Path(dossier)/"donnes.csv" # On recupere le chemin du dossier sélectionné
 
-    with open (track,"w",newline ="",encoding="utf-8") as f:
-        ecrire = csv.writer (f,delimiter=",")
-        ecrire.writerow(["Age","Nom","Ville"])
-        ecrire.writerow([22,"Axel","Nancy"])
+    with open (track,"w",newline ="",encoding="utf-8") as f: # Création et ouverture du fichier f encoder en string en mode écriture
+        ecrire = csv.writer (f,delimiter=",") # Variable d'écriture dans le fichier f avec pour delimiter de séparation du csv la ","
+
+        # Remplissage des lignes
+        ecrire.writerow(["Age","Nom","Ville"]) 
+        ecrire.writerow([22,"Axel","Nancy"]) 
         ecrire.writerow([22,"Axel","Nancy"])
 
-    print (f"fichier creer ici : {track}")
+    print (f"fichier creer ici : {track}") #Affichage du chemin du fichier
 
 else :
     print ("aucun dossier sélectionné")
