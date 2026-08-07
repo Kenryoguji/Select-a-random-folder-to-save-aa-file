326843ce5aa76315ce20a895404af72fc8397374
Kenryoguji <axelteuweussie@gmail.com>
2026-08-07 20:24:26 +0200

    new explain

--- save_local.py
+++ save_local.py
@@ -1,4 +1,31 @@
 import csv
 import time
+import customtkinter as ctk
 import tkinter as tk
 from tkinter import filedialog
+from pathlib import Path
+
+screen = ctk.CTk()
+screen.title ("Mon application")
+screen.geometry ("400x400")
+
+def save() :
+    screen.withdraw()
+
+    track = filedialog.askdirectory()
+
+    line = Path(track)/"result"
+
+    if line :
+        with open (line , "w", newline= "", encoding= "utf-8") as my_file:
+            file = csv.writer(my_file,delimiter=",")
+            file.writerow (["N°","chiffre"])
+            for i in range (10) :
+                file.writerow([i,i])
+        print (f"vous avez enregistré ca ici : {line}")
+        
+    screen.deiconify()
+
+ask = ctk.CTkButton (screen,text ="Download",bg_color= "blue", text_color= "white",command = save)
+ask.pack (side = "rigth", padx = 20 , pady = 20)
+screen.mainloop()
