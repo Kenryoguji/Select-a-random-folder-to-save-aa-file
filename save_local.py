59adb48286e7b4d0a6ff2aec9acfff64b3ce9740
Kenryoguji <axelteuweussie@gmail.com>
2026-08-08 22:59:48 +0200

    add checkBox to choose if we want to download file or not

--- save_local.py
+++ save_local.py
@@ -5,33 +5,40 @@ import tkinter as tk
 from tkinter import filedialog
 from pathlib import Path
 
+
 screen = ctk.CTk()
 screen.title ("Mon application")
 screen.geometry ("400x400")
 
 entree = ctk.CTkEntry(screen,placeholder_text= "Inout a number between 1 and 100",fg_color= "white",text_color="black",width = 220 ,height= 50, corner_radius= 10)
-lab = ctk.CTkCheckBox (screen ,text= "save",bg_color= "white",width = 5, height=5,corner_radius = 2)
+lab = ctk.CTkCheckBox (screen ,text= "save",bg_color= "cyan",text_color= "black",width = 5, height=5,corner_radius = 2)
 
 def save() :
+    verif = lab.get()
     value = entree.get()
     man = int (value)
-    screen.withdraw()
-    track = filedialog.askdirectory()
 
-    line = Path(track)/"result.csv"
-    if line :
-        with open (line , "w", newline= "", encoding= "utf-8") as my_file:
-            file = csv.writer(my_file,delimiter=",")
-            file.writerow (["N°","chiffre"])
-            for i in range (man) :
-                file.writerow([i,i])
-        print (f"vous avez enregistré ca ici : {line}")
+    if verif :
+        screen.withdraw()
+        track = filedialog.askdirectory()
+
+        line = Path(track)/"result.csv"
+        if line :
+            with open (line , "w", newline= "", encoding= "utf-8") as my_file:
+                file = csv.writer(my_file,delimiter=",")
+                file.writerow (["N°","chiffre"])
+                for i in range (man) :
+                    file.writerow([i,i])
+            print (f"vous avez enregistré ca ici : {line}")
         
-    screen.deiconify()
+        screen.deiconify()
+    else :
+        pass
 
 ask = ctk.CTkButton (screen,text ="Download",bg_color= "blue", text_color= "white",command = save)
 
-entree.pack (side="right", padx=30,pady=40)
+entree.pack (side="right", padx=30,pady=40,anchor = "w")
 ask.pack (side = "right", padx=20 , pady=20)
+lab.pack (side = "right", padx=20 , pady=20)
 
 screen.mainloop()
